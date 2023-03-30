eed6b60afdcc0a30d95226c7f9bdcb7e159405b2
hauzenberge <shapovalik26@gmail.com>
2023-03-31 00:27:13 +0300

    update from damin dasboard content

--- app/Http/Controllers/AdminPanel/DashboardController.php
+++ app/Http/Controllers/AdminPanel/DashboardController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\AdminPanel;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin-panel.dashboard', [
+        $view = 'admin-panel.dashboards.' . Auth::user()->role . '-dashboard';
+      //  dd($view);
+        return view($view, [
             'title' => 'Dashboard',
             'active' => 'dashboard'
         ]);
