25c4041dc9c764c037e99173859e5aea82cb8f03
hauzenberge <shapovalik26@gmail.com>
2023-04-01 22:28:03 +0300

    fix google auth bag

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
+use App\Models\User;
+
 class LoginController extends Controller
 {
     public function redirectToGoogle()
