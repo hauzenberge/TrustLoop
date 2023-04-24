38b131a7c8f93671e6d721d29ba57b514911fb3a
hauzenberge <shapovalik26@gmail.com>
2023-04-24 23:48:06 +0300

    update from demo page

--- resources/views/demo_page.blade.php
+++ resources/views/demo_page.blade.php
@@ -9,20 +9,30 @@
     <title>VueJS Bootstrap Form</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- VueJS and Bootstrap JS -->
+
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 
     <!-- BootstrapVue -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-vue/2.21.2/bootstrap-vue.min.js" integrity="sha512-Z0dNfC81uEXC2iTTXtE0rM18I3ATkwn1m8Lxe0onw/uPEEkCmVZd+H8GTeYGkAZv50yvoSR5N3hoy/Do2hNSkw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-vue/2.21.2/bootstrap-vue.min.js"
+        integrity="sha512-Z0dNfC81uEXC2iTTXtE0rM18I3ATkwn1m8Lxe0onw/uPEEkCmVZd+H8GTeYGkAZv50yvoSR5N3hoy/Do2hNSkw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <style>
         .b-rating-icon {
             font-size: 43.3px;
             line-height: 41.35px;
         }
+
 
         #demo-form {
             display: flex;
@@ -54,16 +64,18 @@
             font-size: 36px;
             line-height: 49px;
         }
-    </style>
 
+        #app {
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
+
     <div class="container">
         <div class="row">
-            <div id="app" style="
-                                text-align: center;
-                                ">
+            <div id="app">
                 <!-- Centered text -->
                 <div class="justify-content-center mb-4">
                     <h2>What is Lorem Ipsum?</h2>
@@ -83,43 +95,57 @@
                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
                         Leave a review
                     </button>
+
                     <!-- Bootstrap Modal -->
                     <div class="modal" id="myModal">
                         <div class="modal-dialog">
                             <div class="modal-content" id="demo-content">
                                 <form @submit.prevent="onSubmit">
                                     <div v-for="(value, key) in questions">
-                                        <div v-if="(value.text === 'Rate Us') && (value.type === 'rating')" v-bind="rateAs_index = key">
+                                        <div v-if="(value.text === 'Rate Us') && (value.type === 'rating')"
+                                            v-bind="rateAs_index = key">
                                             <div class="form-group d-flex flex-column">
                                                 <label for="rating" class="text-center mb-2" id="form-label">
-                                                    @{{ value.text }}
+                                                    @{{ value.text }} 
                                                 </label>
-                                                <b-form-rating id="rating" size="sm" variant="warning" no-border v-model="questions[key].value" :max="5" :inline="true"></b-form-rating>
+
+                                                <b-form-rating v-bind:id="value.question_id" size="sm" variant="warning" color="#C2C2C2"
+                                                    no-border v-model="questions[key].value" :max="5" :inline="true">
+                                                </b-form-rating>
                                             </div>
                                         </div>
+
                                         <div v-else>
-                                            <div v-if="(questions[rateAs_index].value < 5) && (questions[rateAs_index].value > 0)">
-                                                <div class="form-group d-flex flex-column" v-if="(value.type === 'rating')">
+                                            <div
+                                                v-if="(questions[rateAs_index].value < 5) && (questions[rateAs_index].value > 0)">
+                                                <div class="form-group d-flex flex-column"
+                                                    v-if="(value.type === 'rating')">
                                                     <label for="rating" class="text-center mb-2">
                                                         @{{ value.text }}
                                                     </label>
-                                                    <b-form-rating id="rating" size="sm" variant="warning" no-border v-model="questions[key].value" :max="5" :inline="true"></b-form-rating>
+                                                    <b-form-rating v-bind:id=" value.question_id" size="sm" color="#C2C2C2" variant="warning" no-border
+                                                        v-model="questions[key].value" :max="5"
+                                                        :inline="true"></b-form-rating>
                                                 </div>
-                                                <div class="form-group d-flex flex-column" v-if="value.type === 'question'">
-                                                    <label for="message" class="text-center mb-2">@{{ value.text }}</label>
-                                                    <textarea class="form-control" v-model="questions[key].value" id="message" rows="3"></textarea>
+
+                                                <div class="form-group d-flex flex-column"
+                                                    v-if="value.type === 'question'">
+                                                    <label for="message" class="text-center mb-2">
+                                                        @{{ value.text }}
+                                                    </label>
+
+                                                    <textarea class="form-control" v-model="questions[key].value"
+                                                        id="message" rows="3"></textarea>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="form-group text-center" v-if="questions[rateAs_index]?.value > 0" style="padding: 45px;">
 
+                                    <div class="form-group text-center" v-if="questions[rateAs_index]?.value > 0"
+                                        style="padding: 45px;">
                                         <button type="submit" class="btn btn-primary" id="btn-submit">
-
                                             @{{ buttonText }}
-
                                         </button>
-
                                     </div>
                                 </form>
                             </div>
@@ -143,15 +169,40 @@
                 buttonText: '',
                 rateAs_index: null
             },
+            watch: {
+                questions: {
+                    handler(newQuestions) {
+                        newQuestions.forEach(question => {
+                            if (question.type === 'rating') {
+                                const ratingValue = question.value;
+                                const ratingComponent = document?.getElementById(question.question_id);
+                                const ratingStars = ratingComponent?.querySelectorAll('.b-rating-icon');
+                               // console.log(ratingStars);
+
+                                ratingStars.forEach((star, index) => {
+                                    if (index < ratingValue) {
+                                        star.style.color = 'rgba(255, 222, 49, 1)';
+                                    } else {
+                                        star.style.background = '';
+                                    }
+                                });
+
+                            }
+                        });
+                    },
+                    deep: true
+                }
+            }
+            ,
+
             methods: {
-                onSubmit: function() {
+                onSubmit: function () {
                     let formData = new FormData();
                     formData.append('answers', this.questions);
                     axios.post(BASE_URL + +this.survey.id + '/responses', {
-                            answers: this.questions.filter(question => question.value !== null)
-                        })
+                        answers: this.questions.filter(question => question.value !== null)
+                    })
                         .then(response => {
-                            console.log(response.data.redirect_url);
                             if (response.data.redirect_url != null) {
                                 window.location.assign(response.data.redirect_url);
                             }
@@ -180,8 +231,8 @@
                 }
                 fetchData();
             },
-
         });
     </script>
 </body>
+
 </html>
