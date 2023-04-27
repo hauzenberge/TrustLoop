3c6a165e8eadcb54f10f0f18053940c1c7c5aaed
hauzenberge <shapovalik26@gmail.com>
2023-04-27 14:19:03 +0300

    update gtom demo scrpt

--- resources/views/admin-panel/dashboards/demo-html.blade.php
+++ resources/views/admin-panel/dashboards/demo-html.blade.php
@@ -26,6 +26,8 @@
 <p class='m-0'>        .b-rating-icon {</p>
 <p class='m-0'>            font-size: 43.3px;</p>
 <p class='m-0'>            line-height: 41.35px;</p>
+<p class='m-0'>            width: 39.95px;</p>
+<p class='m-0'>            height: 38.15px;</p>
 <p class='m-0'>        }</p>
 
 <p class='m-0'>        #modal_button {</p>
@@ -38,6 +40,11 @@
 
 <p class='m-0'>        }</p>
 
+<p class='m-0'>        #btn-submit {</p>
+<p class='m-0'>            border-color: white;</p>
+<p class='m-0'>            background: #5083C1;</p>
+<p class='m-0'>            border-radius: 4px;</p>
+<p class='m-0'>        }</p>
 
 <p class='m-0'>        #demo-form {</p>
 <p class='m-0'>            display: flex;</p>
@@ -50,7 +57,7 @@
 <p class='m-0'>            top: 123px;</p>
 <p class='m-0'>            flex-direction: column;</p>
 <p class='m-0'>            align-items: center;</p>
-<p class='m-0'>            width: 618px;</p>
+<p class='m-0'>            min-width: 564px;</p>
 <p class='m-0'>            min-height: 373px;</p>
 <p class='m-0'>            border-radius: 10px;</p>
 <p class='m-0'>            justify-content: center;</p>
@@ -68,6 +75,34 @@
 <p class='m-0'>            font-weight: 700;</p>
 <p class='m-0'>            font-size: 36px;</p>
 <p class='m-0'>            line-height: 49px;</p>
+<p class='m-0'>        }</p>
+
+<p class='m-0'>        #form-question {</p>
+<p class='m-0'>            display: flex;</p>
+<p class='m-0'>            flex-direction: column;</p>
+<p class='m-0'>            padding: 0px;</p>
+<p class='m-0'>            gap: 15px;</p>
+<p class='m-0'>            height: 144px;</p>
+<p class='m-0'>        }</p>
+
+<p class='m-0'>        #label-massege {</p>
+<p class='m-0'>            text-align: left;</p>
+<p class='m-0'>            font-family: &#039;Open Sans&#039;;</p>
+<p class='m-0'>            font-style: normal;</p>
+<p class='m-0'>            font-weight: 700;</p>
+<p class='m-0'>            font-size: 18px;</p>
+<p class='m-0'>            line-height: 24px;</p>
+<p class='m-0'>            color: #0E2238;</p>
+<p class='m-0'>            flex: none;</p>
+<p class='m-0'>            order: 0;</p>
+<p class='m-0'>            flex-grow: 0;</p>
+<p class='m-0'>        }</p>
+
+<p class='m-0'>        #form-textarea {</p>
+<p class='m-0'>            box-sizing: border-box;</p>
+<p class='m-0'>            background: #FFFFFF;</p>
+<p class='m-0'>            border: 1px solid #5083C1;</p>
+<p class='m-0'>            border-radius: 10px;</p>
 <p class='m-0'>        }</p>
 
 <p class='m-0'>        #app {</p>
@@ -110,7 +145,9 @@
 <p class='m-0'>                                        &lt;div v-if=&quot;(value.text === &#039;Rate Us&#039;) &amp;&amp; (value.type === &#039;rating&#039;)&quot; v-bind=&quot;rateAs_index = key&quot;&gt;</p>
 <p class='m-0'>                                            &lt;div class=&quot;form-group d-flex flex-column&quot;&gt;</p>
 <p class='m-0'>                                                &lt;label for=&quot;rating&quot; class=&quot;text-center mb-2&quot; id=&quot;form-label&quot;&gt;</p>
-<p class='m-0'>                                                    @{{ value.text }}</p>
+<p class='m-0'>                                                    &lt;div v-if=&quot;!questions[key].value&quot;&gt;</p>
+<p class='m-0'>                                                        @{{ value.text }}</p>
+<p class='m-0'>                                                    &lt;/div&gt;</p>
 <p class='m-0'>                                                &lt;/label&gt;</p>
 
 <p class='m-0'>                                                &lt;b-form-rating v-bind:id=&quot;value.question_id&quot; size=&quot;sm&quot; variant=&quot;warning&quot; color=&quot;#C2C2C2&quot; no-border v-model=&quot;questions[key].value&quot; :max=&quot;5&quot; :inline=&quot;true&quot;&gt;</p>
@@ -121,18 +158,18 @@
 <p class='m-0'>                                        &lt;div v-else&gt;</p>
 <p class='m-0'>                                            &lt;div v-if=&quot;(questions[rateAs_index].value &lt; 5) &amp;&amp; (questions[rateAs_index].value &gt; 0)&quot;&gt;</p>
 <p class='m-0'>                                                &lt;div class=&quot;form-group d-flex flex-column&quot; v-if=&quot;(value.type === &#039;rating&#039;)&quot;&gt;</p>
-<p class='m-0'>                                                    &lt;label for=&quot;rating&quot; class=&quot;text-center mb-2&quot;&gt;</p>
+<p class='m-0'>                                                    &lt;label for=&quot;rating&quot; class=&quot;mb-2&quot;&gt;</p>
 <p class='m-0'>                                                        @{{ value.text }}</p>
 <p class='m-0'>                                                    &lt;/label&gt;</p>
 <p class='m-0'>                                                    &lt;b-form-rating v-bind:id=&quot; value.question_id&quot; size=&quot;sm&quot; color=&quot;#C2C2C2&quot; variant=&quot;warning&quot; no-border v-model=&quot;questions[key].value&quot; :max=&quot;5&quot; :inline=&quot;true&quot;&gt;&lt;/b-form-rating&gt;</p>
 <p class='m-0'>                                                &lt;/div&gt;</p>
 
-<p class='m-0'>                                                &lt;div class=&quot;form-group d-flex flex-column&quot; v-if=&quot;value.type === &#039;question&#039;&quot;&gt;</p>
-<p class='m-0'>                                                    &lt;label for=&quot;message&quot; class=&quot;text-center mb-2&quot;&gt;</p>
+<p class='m-0'>                                                &lt;div class=&quot;form-group d-flex flex-column&quot; v-if=&quot;value.type === &#039;question&#039;&quot; id=&quot;form-question&quot;&gt;</p>
+<p class='m-0'>                                                    &lt;label id=&quot;label-massege&quot; for=&quot;message&quot; class=&quot;mb-2&quot;&gt;</p>
 <p class='m-0'>                                                        @{{ value.text }}</p>
 <p class='m-0'>                                                    &lt;/label&gt;</p>
 
-<p class='m-0'>                                                    &lt;textarea class=&quot;form-control&quot; v-model=&quot;questions[key].value&quot; id=&quot;message&quot; rows=&quot;3&quot;&gt;&lt;/textarea&gt;</p>
+<p class='m-0'>                                                    &lt;textarea class=&quot;form-control&quot; v-model=&quot;questions[key].value&quot; id=&quot;message&quot; rows=&quot;3&quot; id=&quot;form-textarea&quot;&gt;&lt;/textarea&gt;</p>
 <p class='m-0'>                                                &lt;/div&gt;</p>
 <p class='m-0'>                                            &lt;/div&gt;</p>
 <p class='m-0'>                                        &lt;/div&gt;</p>
@@ -176,9 +213,14 @@
 <p class='m-0'>                                // console.log(ratingStars);</p>
 
 <p class='m-0'>                                ratingStars.forEach((star, index) =&gt; {</p>
+
 <p class='m-0'>                                    if (index &lt; ratingValue) {</p>
-<p class='m-0'>                                        star.style.color = &#039;rgba(255, 222, 49, 1)&#039;;</p>
+<p class='m-0'>                                        star.innerHTML = &#039;&lt;svg focusable=&quot;false&quot;  class=&quot;bi-star-fill b-icon bi&quot; role=&quot;img&quot; aria-label=&quot;star fill&quot; width=&quot;39.95&quot; height=&quot;38.15&quot; viewBox=&quot;0 0 40 39&quot; fill=&quot;currentColor&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot;&gt;&lt;path d=&quot;M19.9992 31.9622L10.3172 37.7947C9.88948 38.0669 9.44233 38.1835 8.97573 38.1446C8.50912 38.1057 8.10085 37.9502 7.7509 37.678C7.40095 37.4058 7.12876 37.0652 6.93435 36.6562C6.73993 36.2471 6.70105 35.7906 6.8177 35.2867L9.384 24.2632L0.810211 16.856C0.421377 16.506 0.178744 16.1071 0.0823137 15.6591C-0.0141172 15.2112 0.0146564 14.7741 0.168635 14.348C0.324168 13.9203 0.557469 13.5703 0.868536 13.2981C1.1796 13.0259 1.60732 12.851 2.15169 12.7732L13.4668 11.7817L17.8411 1.3998C18.0356 0.933203 18.3373 0.583251 18.7463 0.349951C19.1554 0.11665 19.573 0 19.9992 0C20.4269 0 20.8445 0.11665 21.252 0.349951C21.6595 0.583251 21.9612 0.933203 22.1572 1.3998L26.5316 11.7817L37.8466 12.7732C38.391 12.851 38.8187 13.0259 39.1298 13.2981C39.4409 13.5703 39.6742 13.9203 39.8297 14.348C39.9852 14.7757 40.0148 15.2135 39.9184 15.6615C39.8219 16.1094 39.5785 16.5076 39.1881 16.856L30.6143 24.2632L33.1806 35.2867C33.2973 35.7922 33.2584 36.2494 33.064 36.6585C32.8696 37.0676 32.5974 37.4074 32.2474 37.678C31.8975 37.9502 31.4892 38.1057 31.0226 38.1446C30.556 38.1835 30.1089 38.0669 29.6811 37.7947L19.9992 31.9622Z&quot; fill=&quot;url(#paint0_linear_102_3018)&quot;/&gt;&lt;defs&gt;&lt;linearGradient id=&quot;paint0_linear_102_3018&quot; x1=&quot;0.0774741&quot; y1=&quot;6.15552&quot; x2=&quot;37.9489&quot; y2=&quot;43.9589&quot; gradientUnits=&quot;userSpaceOnUse&quot;&gt;&lt;stop stop-color=&quot;#FFDE31&quot;/&gt;&lt;stop offset=&quot;1&quot; stop-color=&quot;#FFB21E&quot;/&gt;&lt;stop offset=&quot;1&quot; stop-color=&quot;#FF6F1E&quot;/&gt;&lt;/linearGradient&gt;&lt;/defs&gt;&lt;/svg&gt;&#039;;</p>
+<p class='m-0'>                                        // star.style.color = &#039;rgba(255, 222, 49, 1)&#039;;</p>
 <p class='m-0'>                                    } else {</p>
+
+<p class='m-0'>                                        console.log(star.innerHTML);</p>
+<p class='m-0'>                                        star.innerHTML = &#039;&lt;svg viewBox=&quot;0 0 16 16&quot; width=&quot;39.95&quot; height=&quot;38.15&quot; focusable=&quot;false&quot; role=&quot;img&quot; aria-label=&quot;star&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot; fill=&quot;currentColor&quot; class=&quot;bi-star b-icon bi&quot;&gt;&lt;g&gt;&lt;path d=&quot;M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z&quot;&gt;&lt;/path&gt;&lt;/g&gt;&lt;/svg&gt;&#039;;</p>
 <p class='m-0'>                                        star.style.background = &#039;&#039;;</p>
 <p class='m-0'>                                    }</p>
 <p class='m-0'>                                });</p>
--- resources/views/demo_page.blade.php
+++ resources/views/demo_page.blade.php
@@ -26,6 +26,8 @@
         .b-rating-icon {
             font-size: 43.3px;
             line-height: 41.35px;
+            width: 39.95px;
+            height: 38.15px;
         }
 
         #modal_button {
@@ -38,6 +40,11 @@
 
         }
 
+        #btn-submit {
+            border-color: white;
+            background: #5083C1;
+            border-radius: 4px;
+        }
 
         #demo-form {
             display: flex;
@@ -50,7 +57,7 @@
             top: 123px;
             flex-direction: column;
             align-items: center;
-            width: 618px;
+            min-width: 564px;
             min-height: 373px;
             border-radius: 10px;
             justify-content: center;
@@ -68,6 +75,34 @@
             font-weight: 700;
             font-size: 36px;
             line-height: 49px;
+        }
+
+        #form-question {
+            display: flex;
+            flex-direction: column;
+            padding: 0px;
+            gap: 15px;
+            height: 144px;
+        }
+
+        #label-massege {
+            text-align: left;
+            font-family: 'Open Sans';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 18px;
+            line-height: 24px;
+            color: #0E2238;
+            flex: none;
+            order: 0;
+            flex-grow: 0;
+        }
+
+        #form-textarea {
+            box-sizing: border-box;
+            background: #FFFFFF;
+            border: 1px solid #5083C1;
+            border-radius: 10px;
         }
 
         #app {
@@ -109,8 +144,10 @@
                                     <div v-for="(value, key) in questions">
                                         <div v-if="(value.text === 'Rate Us') && (value.type === 'rating')" v-bind="rateAs_index = key">
                                             <div class="form-group d-flex flex-column">
-                                                <label for="rating" class="text-center mb-2" id="form-label" v-if="!questions[key].value">
-                                                    @{{ value.text }}
+                                                <label for="rating" class="text-center mb-2" id="form-label">
+                                                    <div v-if="!questions[key].value">
+                                                        @{{ value.text }}
+                                                    </div>
                                                 </label>
 
                                                 <b-form-rating v-bind:id="value.question_id" size="sm" variant="warning" color="#C2C2C2" no-border v-model="questions[key].value" :max="5" :inline="true">
@@ -121,29 +158,25 @@
                                         <div v-else>
                                             <div v-if="(questions[rateAs_index].value < 5) && (questions[rateAs_index].value > 0)">
                                                 <div class="form-group d-flex flex-column" v-if="(value.type === 'rating')">
-                                                    <label for="rating" class="text-center mb-2">
+                                                    <label for="rating" class="mb-2">
                                                         @{{ value.text }}
                                                     </label>
                                                     <b-form-rating v-bind:id=" value.question_id" size="sm" color="#C2C2C2" variant="warning" no-border v-model="questions[key].value" :max="5" :inline="true"></b-form-rating>
                                                 </div>
 
-                                                <div class="form-group d-flex flex-column" v-if="value.type === 'question'">
-                                                    <label for="message" class="text-center mb-2">
+                                                <div class="form-group d-flex flex-column" v-if="value.type === 'question'" id="form-question">
+                                                    <label id="label-massege" for="message" class="mb-2">
                                                         @{{ value.text }}
                                                     </label>
 
-                                                    <textarea class="form-control" v-model="questions[key].value" id="message" rows="3"></textarea>
+                                                    <textarea class="form-control" v-model="questions[key].value" id="message" rows="3" id="form-textarea"></textarea>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div class="form-group text-center" v-if="questions[rateAs_index]?.value > 0" style="padding: 45px;">
-                                        <button type="submit" class="btn btn-primary" id="btn-submit" style="
-                                                                                            border-color: white;
-                                                                                            background: #5083C1;
-                                                                                            border-radius: 4px;
-                                                                                        ">
+                                        <button type="submit" class="btn btn-primary" id="btn-submit">
                                             @{{ buttonText }}
                                         </button>
                                     </div>
@@ -180,9 +213,14 @@
                                 // console.log(ratingStars);
 
                                 ratingStars.forEach((star, index) => {
+
                                     if (index < ratingValue) {
-                                        star.style.color = 'rgba(255, 222, 49, 1)';
+                                        star.innerHTML = '<svg focusable="false"  class="bi-star-fill b-icon bi" role="img" aria-label="star fill" width="39.95" height="38.15" viewBox="0 0 40 39" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M19.9992 31.9622L10.3172 37.7947C9.88948 38.0669 9.44233 38.1835 8.97573 38.1446C8.50912 38.1057 8.10085 37.9502 7.7509 37.678C7.40095 37.4058 7.12876 37.0652 6.93435 36.6562C6.73993 36.2471 6.70105 35.7906 6.8177 35.2867L9.384 24.2632L0.810211 16.856C0.421377 16.506 0.178744 16.1071 0.0823137 15.6591C-0.0141172 15.2112 0.0146564 14.7741 0.168635 14.348C0.324168 13.9203 0.557469 13.5703 0.868536 13.2981C1.1796 13.0259 1.60732 12.851 2.15169 12.7732L13.4668 11.7817L17.8411 1.3998C18.0356 0.933203 18.3373 0.583251 18.7463 0.349951C19.1554 0.11665 19.573 0 19.9992 0C20.4269 0 20.8445 0.11665 21.252 0.349951C21.6595 0.583251 21.9612 0.933203 22.1572 1.3998L26.5316 11.7817L37.8466 12.7732C38.391 12.851 38.8187 13.0259 39.1298 13.2981C39.4409 13.5703 39.6742 13.9203 39.8297 14.348C39.9852 14.7757 40.0148 15.2135 39.9184 15.6615C39.8219 16.1094 39.5785 16.5076 39.1881 16.856L30.6143 24.2632L33.1806 35.2867C33.2973 35.7922 33.2584 36.2494 33.064 36.6585C32.8696 37.0676 32.5974 37.4074 32.2474 37.678C31.8975 37.9502 31.4892 38.1057 31.0226 38.1446C30.556 38.1835 30.1089 38.0669 29.6811 37.7947L19.9992 31.9622Z" fill="url(#paint0_linear_102_3018)"/><defs><linearGradient id="paint0_linear_102_3018" x1="0.0774741" y1="6.15552" x2="37.9489" y2="43.9589" gradientUnits="userSpaceOnUse"><stop stop-color="#FFDE31"/><stop offset="1" stop-color="#FFB21E"/><stop offset="1" stop-color="#FF6F1E"/></linearGradient></defs></svg>';
+                                        // star.style.color = 'rgba(255, 222, 49, 1)';
                                     } else {
+
+                                        console.log(star.innerHTML);
+                                        star.innerHTML = '<svg viewBox="0 0 16 16" width="39.95" height="38.15" focusable="false" role="img" aria-label="star" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-star b-icon bi"><g><path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path></g></svg>';
                                         star.style.background = '';
                                     }
                                 });
