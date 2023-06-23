bebe63ee07ec8e205eb793dc2167cc2d11c81e78
hauzenberge <shapovalik26@gmail.com>
2023-06-23 19:18:22 +0300

    fix from forgot password blade

--- resources/views/auth2/forgot-password.blade.php
+++ resources/views/auth2/forgot-password.blade.php
@@ -2,51 +2,53 @@
 
 @section('content')
 
-<div class="row align-items-center justify-content-center vh-100">
-    <div class="col-11 col-sm-8 col-md-6 col-lg-5 col-xl-4 col-xxl-3 py-6">
 
-        <!-- Title -->
-        <h1 class="mb-2 text-center">
-            Forgot password?
-        </h1>
 
-        <!-- Subtitle -->
-        <p class="text-secondary text-center">
-            Enter your email address and we'll send you an email with instructions to reset your password
-        </p>
+<!-- Brand -->
+<a class="d-none d-sm-block navbar-brand position-absolute top-0 start-0 pt-1 ps-1 mt-5 ms-5" href="./index.html">
+    <img src="./assets/images/logo.svg" class="navbar-brand-img logo-light logo-large" alt="..." width="125" height="25">
+</a>
 
-        <!-- Form -->
+<!-- Title -->
+<h1 class="mb-2 text-center">
+    Forgot password?
+</h1>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<!-- Subtitle -->
+<p class="text-gray-500 text-center">
+    Enter your email address and we'll send you an email with instructions to reset your password
+</p>
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+<!-- Form -->
 
-            <div class="row">
-                <div class="col-12">
-                    <div class="mb-4">
-                        <label class="form-label" for="email">{{ __('Email Address') }}</label>
-                        <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                    </div>
-                </div>
+<!-- Session Status -->
+<x-auth-session-status class="mb-4" :status="session('status')" />
 
-                <div class="row align-items-center text-center">
-                    <div class="col-12">
-
-                        <!-- Button -->
-                        <button type="submit" class="btn w-100 btn-primary mt-3 mb-2">{{ __('Reset password') }}</button>
-                    </div>
-
-                    <div class="col-12">
-
-                        <!-- Link -->
-                        <small class="mb-0 text-muted">{{ __('Back to') }} <a href="{{ url('/login') }}" class="fw-semibold">{{ __('Sign in') }}</a></small>
-                    </div>
-                </div> <!-- / .row -->
+<form method="POST" action="{{ route('password.email') }}">
+    @csrf
+    <div class="row">
+        <div class="col-12">
+            <div class="mb-4">
+                <label class="form-label" for="email">{{ __('Email Address') }}</label>
+                <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
-        </form>
+        </div>
+
+        <div class="row align-items-center text-center">
+            <div class="col-12">
+
+                <!-- Button -->
+                <button type="submit" class="btn w-100 btn-primary mt-3 mb-2">{{ __('Reset password') }}</button>
+            </div>
+
+            <div class="col-12">
+
+                <!-- Link -->
+                <small class="mb-0 text-muted">{{ __('Back to') }} <a href="{{ url('/login') }}" class="fw-semibold">{{ __('Sign in') }}</a></small>
+            </div>
+        </div> <!-- / .row -->
     </div>
-</div>
+</form>
+
 @endsection
