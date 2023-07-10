4ffb301e3a3bb9f5befb384719e123acd25f5492
hauzenberge <shapovalik26@gmail.com>
2023-07-10 19:48:58 +0300

    fix bag

--- app/Http/Controllers/API/SurveyController.php
+++ app/Http/Controllers/API/SurveyController.php
@@ -10,9 +10,31 @@ use App\Models\SurveyResponse;
 
 class SurveyController extends Controller
 {
+    private function questionList($user)
+    {
+        $survey = $user->survey()->first();
+        $questions = $user->survey()
+            ->with('questions')
+            ->first()
+            ->questions
+            ->map(function ($item) {
+                return [
+                    'question_id' => $item->id,
+                    'text' => $item->text,
+                    'type' => $item->type,
+                    'survey_id' => $item->survey_id,
+                    'value' => null
+                ];
+            });
+        return  collect([
+            'survey' => $survey,
+            'questions' => $questions
+        ])->toJson();
+    }
+
     public function getSurveysByUser(User $user)
     {
-        $max_requests = intval($user->userData->plan->max_request);
+        $max_requests = $user->userData->plan->max_request;
         $user_responce = SurveyResponse::where('user_id', $user->id)->first();
 
         if ($user_responce == null) {
@@ -20,28 +42,13 @@ class SurveyController extends Controller
                 'user_id' => $user->id,
                 'sum_requests' => 0
             ]);
-        }
+        }        
 
-        if ($user_responce->sum_requests != $max_requests) {
-            $survey = $user->survey()->first();
-            $questions = $user->survey()
-                ->with('questions')
-                ->first()
-                ->questions
-                ->map(function ($item) {
-                    return [
-                        'question_id' => $item->id,
-                        'text' => $item->text,
-                        'type' => $item->type,
-                        'survey_id' => $item->survey_id,
-                        'value' => null
-                    ];
-                });
-            $return = collect([
-                    'survey' => $survey,
-                    'questions' => $questions
-                ])->toJson();
-        }else $return = collect([
+        if ($max_requests === "unlimited") {
+            $return = $this->questionList($user);
+        }elseif ($user_responce->sum_requests != intval($max_requests)) {
+            $return = $this->questionList($user);
+        } else $return = collect([
             'massege' => 'Error'
         ])->toJson();
 
