731be958d7e77ad220127dd530cbe7214e45f72b
hauzenberge <shapovalik26@gmail.com>
2023-07-10 20:02:45 +0300

    update from User Reviews task

--- app/Http/Controllers/AdminPanel/CSVController.php
+++ app/Http/Controllers/AdminPanel/CSVController.php
@@ -12,67 +12,67 @@ use App\Models\Answer;
 class CSVController extends Controller
 {
     public function export()
-{
-    $answers = Answer::where('user_id', Auth::user()->id)
-        ->with('question')
-        ->get()
-        ->groupBy(function ($answer) {
-            return $answer->created_at->format('d.m.Y h:m:s');
-        })
-        ->map(function ($item, $key) {
-            $rate_as = $item->filter(function ($item) {
-                return $item->question->text == "Rate Us";
-            })->first();
-
-            if ($rate_as) {
-                $rate_as = $rate_as->text;
-            } else {
-                $rate_as = '0';
-            }
-
-            $all_questions = $item->filter(function ($item) {
-                return $item->question->text != "Rate Us";
+    {
+        $answers = Answer::where('user_id', Auth::user()->id)
+            ->with('question')
+            ->get()
+            ->groupBy(function ($answer) {
+                return $answer->created_at->format('d.m.Y h:m:s');
             })
-                ->values()
-                ->map(function ($item) {
-                    return [
-                        'question' => $item->question->text,
-                        'answer' => $item->text
-                    ];
+            ->map(function ($item, $key) {
+                $rate_as = $item->filter(function ($item) {
+                    return $item->question->text == "Rate Us";
+                })->first();
+
+                if ($rate_as) {
+                    $rate_as = $rate_as->text;
+                } else {
+                    $rate_as = '0';
+                }
+
+                $all_questions = $item->filter(function ($item) {
+                    return $item->question->text != "Rate Us";
                 })
-                ->toArray();
+                    ->values()
+                    ->map(function ($item) {
+                        return $item->question->text . ' : ' . $item->text;
+                    })
+                    ->toArray();
 
-            return [
-                'rate_as' => intval($rate_as),
-                'date' => $key,
-                'all_questions' => $all_questions
-            ];
-        });
+                $all_questions_string = implode(', ', $all_questions);
 
-    $data = [
-        ['Review', 'Date', 'All Questions'],
-    ];
+                // dd($all_questions);
 
-    foreach ($answers as $answer) {
-        $row = [
-            $answer['rate_as'],
-            $answer['date'],
-            json_encode($answer['all_questions']),
+                return [
+                    'rate_as' => intval($rate_as),
+                    'date' => $key,
+                    'all_questions' => $all_questions_string
+                ];
+            });
+
+        $data = [
+            ['Review', 'Date', 'All Questions'],
         ];
 
-        $data[] = $row;
+        foreach ($answers as $answer) {
+            $row = [
+                $answer['rate_as'],
+                $answer['date'],
+                json_encode($answer['all_questions']),
+            ];
+
+            $data[] = $row;
+        }
+
+        $csv = Writer::createFromString('');
+        $csv->insertAll($data);
+
+        $filename = 'export.csv';
+        $headers = [
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => 'attachment; filename="' . $filename . '"',
+        ];
+
+        return response($csv->getContent(), 200, $headers);
     }
-
-    $csv = Writer::createFromString('');
-    $csv->insertAll($data);
-
-    $filename = 'export.csv';
-    $headers = [
-        'Content-Type' => 'text/csv',
-        'Content-Disposition' => 'attachment; filename="' . $filename . '"',
-    ];
-
-    return response($csv->getContent(), 200, $headers);
-}
-
 }
